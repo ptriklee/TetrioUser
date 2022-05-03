22b29480f8c59c3ef4c5d962ef7fa40163e65936
Patrick <ptriklee@gmail.com>
2022-05-03 13:01:26 -0700

    Backend complete for mvp

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+// class App extends React.Component {
+//   render() {
+//       return <h1>Hello, World!</h1>
+//   }
+// }
 
-class App extends React.Component {
-  render() {
-      return <h1>Hello, World!</h1>
+function App () {
+  
+
+  const getGenStats = () => {
+    axios.get('/general')
+    .then(res => {
+      console.log('data >>>', res.data.data)
+    })
+    .catch(err => {
+      console.log('no good', err)
+    })
   }
+
+  return (
+    <div>
+    <h1>Hello, World!</h1>
+    <button onClick={getGenStats}></button>
+    </div>
+  )
 }
+
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,45 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
+// const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-//express.static middleware
 app.use(express.static(__dirname + '/../client/dist'));
+
+// app.use(cors({
+//   origin: `http://localhost:${PORT}`,
+//   credentials: true,
+//   })
+// )
+
+app.get('/general', (req, res) => {
+  axios.get('https://ch.tetr.io/api/general/stats')
+    .then(response => {
+      // console.log('data >>', response.data.data)
+      res.send(response.data)
+    })
+    .catch(err => {
+      console.log('err >>', err)
+      res.send(err)
+    })
+})
+
+app.get('/users', (req, res) => {
+  const name = req.body.name;
+  axios.get(`https://ch.tetr.io/api/users/${name}`)
+    .then(response => {
+      console.log('data >>', response.data.data)
+      res.send(response.data)
+    })
+    .catch(err => {
+      console.log('err >>', err)
+      res.send(err)
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);

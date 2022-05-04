ce26ca4acd199505c8b2aafcf5b55b33fbf3ab31
Patrick <ptriklee@gmail.com>
2022-05-03 21:19:43 -0700

    ReactDom legacy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,10 @@
+// import ReactDOM from 'react-dom';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const rootElement = document.getElementById('app');
+const root = createRoot(rootElement);
+
+root.render(<App />);
+// ReactDOM.render(<App />, document.getElementById('app'));

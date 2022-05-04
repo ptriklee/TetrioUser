773d201e1cdcf4a612fa508a64a6f84779bec2ed
Patrick <ptriklee@gmail.com>
2022-05-04 00:47:31 -0700

    React bootstrap partial implementation

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import {Button, TextField, Alert, Grid} from '@mui/material';
+import { Button, Row, Col, Container, Form, Alert } from 'react-bootstrap';
 
 function App () {
-  const [playerCount, setPlayerCount] = useState(0);
+  const [playerCount, setPlayerCount] = useState('');
+  const [userName, setUserName] = useState('')
 
   useEffect(() => {
     axios.get('/general')
@@ -16,6 +18,11 @@ function App () {
     })
   }, []);
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUserName(e.target.value)
+  }
+
     //   const timer = window.setInterval(() => {
     //     setPlayerCount(prevTime => prevTime + 1000);
     //   }, 1000);
@@ -26,15 +33,37 @@ function App () {
 
   return (
     <div>
-    <h1>Tetri.no</h1>
-    <div>{playerCount}</div>
-    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }} background-color="linear-gradient(90deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)">
-    <Grid item xs={3}>
-    <TextField label="Text Here" color="primary" background-color="white" margin="normal" />
-    <Button variant="contained">Find Tetr.io User</Button>
-    </Grid>
-    </Grid>
-    <Alert severity="info" color="error">No such user! | Either you mistyped something, or the account no longer exists!</Alert>
+    <Container fluid>
+    <Row className="justify-content-md-center">
+    <Col md="auto"><h1>Tetri.no</h1></Col>
+    </Row>
+    <Row className="justify-content-md-center">
+      {(!playerCount)
+      ?
+    <Col md="auto"><div>{playerCount}</div></Col>
+      :
+    <Col md="auto"><div>{playerCount} live players!</div></Col>
+      }
+    </Row>
+    <Row>
+    <Form>
+    <Form.Label>Username</Form.Label>
+    <Form.Control type="email" placeholder="Enter username" onChange={handleChange}/>
+    <Form.Text>
+      Username must be 3 to 16 characters long /
+      Username may only contain letters, numbers, dashes, and underscores
+    </Form.Text>
+    </Form>
+    <Button variant="success">Search</Button>
+    </Row>
+    <br />
+    <Alert variant="danger">
+    <Alert.Heading>No such User!</Alert.Heading>
+    <p>
+      Either you mistyped something, or the account no longer exists
+    </p>
+    </Alert>
+    </Container>
     </div>
   )
 }
